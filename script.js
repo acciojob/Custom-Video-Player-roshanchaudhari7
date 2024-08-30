59edd1edef95210cee3f874355f46e0b8fc94471
Roshan Chaudhari <98824802+roshanchaudhari7@users.noreply.github.com>
2024-08-30 15:43:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,49 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+video.addEventListener('timeupdate', updateProgress);
+function updateProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+toggle.addEventListener('click', togglePlay);
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+    toggle.textContent = '❚ ❚';
+  } else {
+    video.pause();
+    toggle.textContent = '►';
+  }
+}
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+ranges.forEach(range => range.addEventListener('input', handleRangeUpdate));
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+progress.addEventListener('click', scrub);
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+progress.addEventListener('mousedown', e => e.preventDefault());
+
+
+
+
+
+
+
+
+
+
+
